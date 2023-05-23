16f5152c66f5b55022e40409c11f00308707570b
DESKTOP-P3IC1R0\Semoga Berkah <nikenarrailmaauliya@gmail.com>
2023-05-23 21:47:02 +0700

    bdd manage data (test suite report newsletter)

---
+++ Test Suites/TS-Newsletter.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS-Newsletter</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9884079c-7b09-49d2-96a9-69ab8fa2197c</testSuiteGuid>
+   <testCaseLink>
+      <guid>a841aead-e0ca-4f47-8f6b-9dd460158b89</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Manage Data/cucumber-newsletter</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
