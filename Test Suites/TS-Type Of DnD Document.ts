cdb6f3ea0890317f49daf176f6ec416e3ac8e512
DESKTOP-P3IC1R0\Semoga Berkah <nikenarrailmaauliya@gmail.com>
2023-05-23 22:12:50 +0700

    bdd manage data (test suite report type of dnd document)

---
+++ Test Suites/TS-Type Of DnD Document.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS-Type Of DnD Document</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b559f0f1-79bc-444c-8b78-53c20afc2dd5</testSuiteGuid>
+   <testCaseLink>
+      <guid>92ffda83-3e47-46ee-baf5-dd857c7f0a8e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Manage Data/cucumber-type_of_DnD</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
