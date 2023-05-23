f39e0276b1c7c4aad45cbe2e12ac2227b9922787
DESKTOP-P3IC1R0\Semoga Berkah <nikenarrailmaauliya@gmail.com>
2023-05-23 20:47:06 +0700

    bdd manage data (test suite report login)

---
+++ Test Suites/TS-Login.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS-Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8cf1a355-f4f2-49bc-9473-8c2db0049ef2</testSuiteGuid>
+   <testCaseLink>
+      <guid>49ee10ce-2003-4710-be63-39810c51f245</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/cucumber-login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
