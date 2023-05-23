e6f017d61c3542472737669440456a6055d94d35
DESKTOP-P3IC1R0\Semoga Berkah <nikenarrailmaauliya@gmail.com>
2023-05-23 21:56:08 +0700

    bdd manage data (test suite report pro stand)

---
+++ Test Suites/TS-Pro Stand.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS-Pro Stand</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8587b641-724e-4f4f-baa5-6bf39014891f</testSuiteGuid>
+   <testCaseLink>
+      <guid>c6cbb8d0-e00d-4661-8022-5bc34da6c21d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Manage Data/cucumber-pro_stand</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
